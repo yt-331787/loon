c0a227fe5a47ea51dc29394624f13e4a7c7f3915
yt-331787 <tiamoyinlei@163.com>
2025-03-29 03:49:31 +0800

    Delete cmm.js

--- cmm.js
+++
@@ -1,8 +0,0 @@
- const { _entrance, _geo } = $server
-
-let entranceFlag = ProxyUtils.getFlag(_entrance.countryCode).replace(/🇹🇼/g, '🇼🇸')
-let geoFlag = ProxyUtils.getFlag(_geo.countryCode).replace(/🇹🇼/g, '🇼🇸')
-$server.name = (_entrance.aso !== _geo.aso || _entrance.countryCode !== _geo.countryCode) ? `${entranceFlag} ${_entrance.aso} ➮ ${geoFlag} ${_geo.aso} [${$server.type}]` : `${geoFlag} ${_geo.aso} [${$server.type}]`
-
-delete $server._entrance
-delete $server._geo
